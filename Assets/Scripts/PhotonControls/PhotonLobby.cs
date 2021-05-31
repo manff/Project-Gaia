929613871635011d57fb1f6fee8e0502922455c2
Connor <naiboyz0247@cox.net>
2021-05-30 18:39:07 -0700

    Photon Test Counter
    Used Photon to add a counter in the AR environment that can be incremented from the desktop application.

---
+++ Assets/Scripts/PhotonControls/PhotonLobby.cs
@@ -0,0 +1,90 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    private PhotonView PV;
+
+    public GameObject startButton;
+    public GameObject cancelButton;
+
+    public Text txt;
+    private int counter;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = PhotonView.Get(this);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        startButton.SetActive(true);
+    }
+
+    public void OnStartButtonClicked()
+    {
+        Debug.Log("Start Button was clicked");
+        startButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random room but failed. There must be no open room available");
+        CreatRoom();
+    }
+
+    void CreatRoom()
+    {
+        Debug.Log("Trying to create a new room");
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("We are now in a room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to creat a room but failed, there must already be a room with the same name");
+        CreatRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel Button was clicked");
+        cancelButton.SetActive(false);
+        startButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+
+    [PunRPC]
+    void ChangeText()
+    {
+        txt.text = "Test Text:" + counter;
+        counter++;
+    }
+
+    public void OnCounterButtonClicked()
+    {
+        PV.RPC("ChangeText", RpcTarget.All);
+    }
+}
