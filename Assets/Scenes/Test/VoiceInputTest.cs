653171ed2f24d9d191514e01342f89e5fa69d2ff
mosesvega <39105853+mosesvega@users.noreply.github.com>
2021-06-01 17:10:26 -0700

    Voice Control
    - Added Voice Control Script

---
+++ Assets/Scenes/Test/VoiceInputTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceInputTest : MonoBehaviour
+{
+    public Transform target;
+
+    public bool shouldShow = true;
+
+    void Update()
+    {
+        if (!shouldShow)
+        {
+            target.localScale = Vector3.Lerp(target.localScale, Vector3.zero, Time.deltaTime * 10);
+        } else
+        {
+            target.localScale = Vector3.Lerp(target.localScale, Vector3.one, Time.deltaTime * 10);
+        }
+    }
+
+    public void Show()
+    {
+        shouldShow = true;
+    }
+
+    public void Hide()
+    {
+        shouldShow = false;
+    }
+}
---
+++ Assets/VoiceInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+
+public class VoiceInput : MonoBehaviour
+{
+    [System.Serializable]
+    public class VoiceCommand {
+        public string keyword;
+        public UnityEvent action;
+    }
+
+    public List<VoiceCommand> commands;
+    private Dictionary<string, VoiceCommand> commandsTable = new Dictionary<string, VoiceCommand>();
+    KeywordRecognizer keywordRecognizer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+        for (int i = 0; i < commands.Count; i++)
+        {
+            VoiceCommand command = commands[i];
+            commandsTable[command.keyword] = command;
+            keywords.Add(command.keyword, () =>
+            {
+            });
+        }
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log(args.text);
+        VoiceCommand command;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (commandsTable.TryGetValue(args.text, out command))
+        {
+            command.action.Invoke();
+        }
+    }
+}
